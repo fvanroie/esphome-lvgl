b5e7b836fa4ee5534ddff12f2f9fe104945c74a0
fvanroie <15969459+fvanroie@users.noreply.github.com>
2021-09-15 17:11:05 +0200

    Test Labon L8

--- LvglComponent.h
+++ LvglComponent.h
@@ -6,6 +6,9 @@
 #include "tft_espi.h"
 #include "bootlogo.h"
 
+#include "esphome/core/component.h"
+#include "esphome/components/number/number.h"
+
 const size_t buf_pix_count = LV_HOR_RES_MAX * LV_VER_RES_MAX / 5;
 
 static lv_disp_draw_buf_t disp_buf;
@@ -147,3 +150,313 @@ void IRAM_ATTR my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *d
     // Serial.println(touchY);
   }
 }
+
+class LvglCheckbox : public Component, public Switch
+{
+private:
+  lv_coord_t x;
+  lv_coord_t y;
+  lv_coord_t w;
+  lv_coord_t h;
+
+public:
+  // constructor
+  lv_obj_t *obj = NULL;
+  LvglCheckbox(lv_coord_t _x, lv_coord_t _y, lv_coord_t _w, lv_coord_t _h)
+  {
+    x = _x;
+    y = _y;
+    w = _w;
+    h = _h;
+  }
+
+  void setup() override
+  {
+    // This will be called by App.setup()
+    obj = lv_checkbox_create(lv_scr_act());
+    lv_obj_set_pos(obj, x, y);
+    lv_obj_set_size(obj, w, h);
+    lv_checkbox_set_text(obj, (this)->get_name().c_str());
+
+    // Set Callback
+    lv_obj_add_event_cb(obj, lvgl_event_cb, LV_EVENT_VALUE_CHANGED, (void *)this);
+  }
+
+  void write_state(bool state) override
+  {
+    // This will be called every time the user requests a state change.
+    ((state) ? lv_obj_add_state(obj, LV_STATE_CHECKED) : lv_obj_clear_state(obj, LV_STATE_CHECKED));
+
+    // Acknowledge new state by publishing it
+    publish_state(state);
+  }
+
+  static void lvgl_event_cb(lv_event_t *event)
+  {
+    lv_obj_t *target = lv_event_get_target(event);
+    LvglCheckbox *sw = (LvglCheckbox *)event->user_data;
+
+    // printf("Clicked\n");
+    bool state = (lv_obj_get_state(target) & LV_STATE_CHECKED);
+
+    // Acknowledge new state by publishing it
+    sw->publish_state(state);
+  }
+};
+
+class LvglSwitch : public Component, public Switch
+{
+private:
+  lv_coord_t x;
+  lv_coord_t y;
+  lv_coord_t w;
+  lv_coord_t h;
+
+public:
+  // constructor
+  lv_obj_t *obj = NULL;
+  LvglSwitch(lv_coord_t _x, lv_coord_t _y, lv_coord_t _w, lv_coord_t _h)
+  {
+    x = _x;
+    y = _y;
+    w = _w;
+    h = _h;
+  }
+
+  void setup() override
+  {
+    // This will be called by App.setup()
+    obj = lv_switch_create(lv_scr_act());
+    lv_obj_set_pos(obj, x, y);
+    lv_obj_set_size(obj, w, h);
+    // lv_checkbox_set_text(obj, (this)->get_name().c_str());
+
+    lv_obj_add_style(obj, &switch_style, 0);
+
+    // Set Callback
+    lv_obj_add_event_cb(obj, lvgl_event_cb, LV_EVENT_VALUE_CHANGED, (void *)this);
+  }
+
+  void write_state(bool state) override
+  {
+    // This will be called every time the user requests a state change.
+    ((state) ? lv_obj_add_state(obj, LV_STATE_CHECKED) : lv_obj_clear_state(obj, LV_STATE_CHECKED));
+
+    // Acknowledge new state by publishing it
+    publish_state(state);
+  }
+
+  static void lvgl_event_cb(lv_event_t *event)
+  {
+    lv_obj_t *target = lv_event_get_target(event);
+    LvglSwitch *sw = (LvglSwitch *)event->user_data;
+
+    // printf("Clicked\n");
+    bool state = (lv_obj_get_state(target) & LV_STATE_CHECKED);
+
+    // Acknowledge new state by publishing it
+    sw->publish_state(state);
+  }
+};
+
+class LvglToggleButton : public Component, public Switch
+{
+private:
+  lv_coord_t x;
+  lv_coord_t y;
+  lv_coord_t w;
+  lv_coord_t h;
+
+public:
+  // constructor
+  lv_obj_t *obj = NULL;
+  lv_obj_t *label = NULL;
+  LvglToggleButton(lv_coord_t _x, lv_coord_t _y, lv_coord_t _w, lv_coord_t _h)
+  {
+    x = _x;
+    y = _y;
+    w = _w;
+    h = _h;
+  }
+
+  void setup() override
+  {
+    // This will be called by App.setup()
+    obj = lv_btn_create(lv_scr_act());
+    lv_obj_add_flag(obj, LV_OBJ_FLAG_CHECKABLE); // enable toggle
+
+    lv_obj_set_pos(obj, x, y);
+    lv_obj_set_size(obj, w, h);
+
+    label = lv_label_create(obj);
+    lv_label_set_text(label, (this)->get_name().c_str());
+    lv_obj_center(label);
+
+    // Set Callback
+    lv_obj_add_event_cb(obj, lvgl_event_cb, LV_EVENT_VALUE_CHANGED, (void *)this);
+  }
+
+  void write_state(bool state) override
+  {
+    // This will be called every time the user requests a state change.
+    ((state) ? lv_obj_add_state(obj, LV_STATE_CHECKED) : lv_obj_clear_state(obj, LV_STATE_CHECKED));
+
+    // Acknowledge new state by publishing it
+    publish_state(state);
+  }
+
+  void set_text(const std::string &text)
+  {
+    if (!label)
+      return;
+    lv_label_set_text(label, text.c_str());
+  }
+
+  static void lvgl_event_cb(lv_event_t *event)
+  {
+    lv_obj_t *target = lv_event_get_target(event);
+    LvglToggleButton *sw = (LvglToggleButton *)event->user_data;
+
+    // printf("Clicked\n");
+    bool state = (lv_obj_get_state(target) & LV_STATE_CHECKED);
+
+    // Acknowledge new state by publishing it
+    sw->publish_state(state);
+  }
+};
+
+class LvglSlider : public number::Number, public Component
+{
+private:
+  lv_coord_t x;
+  lv_coord_t y;
+  lv_coord_t w;
+  lv_coord_t h;
+
+public:
+  // constructor
+  lv_obj_t *obj = NULL;
+  lv_obj_t *label = NULL;
+  LvglSlider(lv_coord_t _x, lv_coord_t _y, lv_coord_t _w, lv_coord_t _h)
+  {
+    x = _x;
+    y = _y;
+    w = _w;
+    h = _h;
+  }
+
+  void setup() override
+  {
+    // This will be called by App.setup()
+    obj = lv_slider_create(lv_scr_act());
+    lv_obj_add_flag(obj, LV_OBJ_FLAG_CHECKABLE); // enable toggle
+
+    lv_obj_set_pos(obj, x, y);
+    lv_obj_set_size(obj, w, h);
+
+    // Set Callback
+    //  lv_obj_add_event_cb(obj, lvgl_event_cb, LV_EVENT_VALUE_CHANGED, (void *)&(this->make_call()));
+  }
+
+protected:
+  void control(float value) override
+  {
+    lv_slider_set_value(obj, value + 0.5, LV_ANIM_ON);
+    this->publish_state(value);
+  }
+
+private:
+  static void lvgl_event_cb(lv_event_t *event)
+  {
+    lv_obj_t *target = lv_event_get_target(event);
+    NumberCall *sw = (NumberCall *)event->user_data;
+
+    // printf("Clicked\n");
+    int32_t x = lv_slider_get_value(target);
+    int32_t min = lv_slider_get_min_value(target);
+    int32_t max = lv_slider_get_max_value(target);
+    int32_t delta = max - min;
+
+    float state = 0.0f;
+    if (delta)
+      state = (float)(x - min) * 100 / (float)delta;
+
+    // Acknowledge new state by publishing it
+    sw->set_value(state);
+    sw->perform();
+  }
+};
+
+class LvglLight : public Component, public LightOutput
+{
+private:
+  lv_coord_t x;
+  lv_coord_t y;
+  lv_coord_t w;
+  lv_coord_t h;
+
+public:
+  // constructor
+  lv_obj_t *obj = NULL;
+  lv_obj_t *label = NULL;
+  LvglLight(lv_coord_t _x, lv_coord_t _y, lv_coord_t _w, lv_coord_t _h)
+  {
+    x = _x;
+    y = _y;
+    w = _w;
+    h = _h;
+  }
+
+  void setup() override
+  {
+    // This will be called by App.setup()
+    obj = lv_slider_create(lv_scr_act());
+
+    lv_obj_set_pos(obj, x, y);
+    lv_obj_set_size(obj, w, h);
+
+    // Set Callback
+    lv_obj_add_event_cb(obj, lvgl_event_cb, LV_EVENT_VALUE_CHANGED, (void *)this);
+  }
+
+  LightTraits get_traits() override
+  {
+    // return the traits this light supports
+    light::LightTraits traits{};
+    traits.set_supported_color_modes({light::ColorMode::BRIGHTNESS});
+    return traits;
+  }
+
+  void write_state(LightState *state) override
+  {
+    // This will be called every time the user requests a state change.
+    // printf("Clicked\n");
+    int32_t x = lv_slider_get_value(obj);
+    int32_t min = lv_slider_get_min_value(obj);
+    int32_t max = lv_slider_get_max_value(obj);
+    int32_t delta = max - min;
+
+    // This will be called by the light to get a new state to be written.
+    float brightness = 0.0f;
+    if (delta)
+      brightness = (float)(x - min) / (float)delta;
+
+    // use any of the provided current_values methods
+    state->current_values_as_brightness(&brightness);
+    print(state.get_name().c_str());
+    // Write red, green and blue to HW
+    // ...
+  }
+
+  static void lvgl_event_cb(lv_event_t *event)
+  {
+    lv_obj_t *target = lv_event_get_target(event);
+    LvglLight *sw = (LvglLight *)event->user_data;
+
+    // printf("Clicked\n");
+    bool state = (lv_obj_get_state(target) & LV_STATE_CHECKED);
+
+    // Acknowledge new state by publishing it
+    //sw->publish_state(state);
+  }
+};
